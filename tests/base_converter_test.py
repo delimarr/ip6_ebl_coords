5a19ef29dac2f67557ee742a875e4c30f3292a61
delimarr <robin.delimar@students.fhnw.ch>
2023-11-14 13:32:06 +0000

    poetry install and pre-commit

--- tests/base_converter_test.py
+++ tests/base_converter_test.py
@@ -1,14 +1,15 @@
-"Test BaseConverter class."
+"""Test BaseConverter class."""
 import socket
 from time import sleep
 
 from ebl_coords.backend.converter.base_converter import BaseConverter
 from ebl_coords.backend.converter.output_dataclass import ConverterOuput
 
-def test_streaming() -> None:
 
+def test_streaming() -> None:
+    """Test connection to BaseConverter Server."""
     c1 = ConverterOuput(0, 1, 1, 1, {})
-    c2 = ConverterOuput(2, 3, 4, 5, {'key': 0.1})
+    c2 = ConverterOuput(2, 3, 4, 5, {"key": 0.1})
 
     ip = "127.0.0.1"
     port = 12701
@@ -20,9 +21,6 @@ def test_streaming() -> None:
     recv_socket.connect((ip, port))
     bc.buffer.append(c1)
     bc.buffer.append(c2)
-    a = recv_socket.recv(1024)
-    b = recv_socket.recv(1024)
-    print()
-
-
-test_streaming()
+    _ = recv_socket.recv(1024)
+    _ = recv_socket.recv(1024)
+    recv_socket.close()

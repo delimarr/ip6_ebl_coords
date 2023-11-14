62f599d0c58b613d7b948a83baa052a6db439a9e
delimarr <robin.delimar@students.fhnw.ch>
2023-11-14 08:33:10 +0100

    pre commit skipped, baseconverter

---
+++ ebl_coords/backend/converter/base_converter.py
@@ -0,0 +1,63 @@
+"""Receive any Input and stream it to a socket."""
+import socket
+import json
+from dataclasses import asdict
+
+from threading import Thread
+
+from typing import List
+from ebl_coords.backend.converter.output_dataclass import ConverterOuput
+
+class BaseConverter():
+    def __init__(
+        self, 
+        ip_server: str = "127.0.0.1", 
+        port_server: int = 42042
+    ) -> None:
+        """Initialize converter base. Start server that streams from self.buffer.
+
+        Args:
+            ip_server (str): ip adress of output socket. Defaults to "127.0.0.1".
+            port_server (int): port of output socket. Defaults to 42042.
+        """
+        self.ip_server: str = ip_server
+        self.port_server: int = port_server
+
+        self.buffer: List[ConverterOuput] = []
+
+        self._start_streaming()
+
+    def read_input(self) -> None:
+        """Read Input and fill self.buffer.
+
+        Raises:
+            NotImplementedError: override this function.
+        """
+        raise NotImplementedError
+
+    def _start_streaming(self) -> None:
+        self.server_thread = Thread(
+            target=self._stream_buffer, 
+            args=(self.ip_server, self.port_server),
+            daemon=True
+        )
+        self.server_thread.start()
+
+    def _stream_buffer(self, ip: str, port: int) -> None:
+        server_socket = socket.socket()
+        server_socket.bind((ip, port))
+        server_socket.listen(10)
+        conn, addr = server_socket.accept()
+        while True:
+            if self.buffer:
+                converter_output = self.buffer.pop()
+                json_b = json.dumps(asdict(converter_output)).encode()
+                conn.send(json_b)
+
+
+    
+
+        
+
+
+    
---
+++ tests/base_converter_test.py
@@ -0,0 +1,28 @@
+"Test BaseConverter class."
+import socket
+from time import sleep
+
+from ebl_coords.backend.converter.base_converter import BaseConverter
+from ebl_coords.backend.converter.output_dataclass import ConverterOuput
+
+def test_streaming() -> None:
+
+    c1 = ConverterOuput(0, 1, 1, 1, {})
+    c2 = ConverterOuput(2, 3, 4, 5, {'key': 0.1})
+
+    ip = "127.0.0.1"
+    port = 12701
+    bc = BaseConverter(ip_server=ip, port_server=port)
+
+    sleep(0.5)
+
+    recv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    recv_socket.connect((ip, port))
+    bc.buffer.append(c1)
+    bc.buffer.append(c2)
+    a = recv_socket.recv(1024)
+    b = recv_socket.recv(1024)
+    print()
+
+
+test_streaming()

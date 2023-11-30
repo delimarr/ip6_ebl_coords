8863f4a4ca494360fffa1edc675fd16ed44ccb0e
delimarr <robin.delimar@students.fhnw.ch>
2023-11-30 19:43:09 +0100

    removed empty file

--- live.py
+++ live.py
@@ -5,7 +5,7 @@ from os import path
 
 from ebl_coords.backend.mock.gt_recorder_original import GtRecorderOriginal
 
-folder = "./data/got_raw_files/run9/"
+folder = "./data/got_raw_files/run10/"
 
 # out_file='dab_ew01_dcc019'
 # out_file='dab_ew02_dcc017'
@@ -13,14 +13,14 @@ folder = "./data/got_raw_files/run9/"
 # out_file='dab_ew04_dcc013'
 # out_file='dab_ew05_dcc014'
 # out_file='dab_ew06_dcc016'
-# out_file='dab_ew07_dcc015'
+out_file = "dab_ew07_dcc015"
 # out_file='dab_ew08_dcc003'
 # out_file='dab_ew09_dcc004'
 # out_file='dab_ew10_dcc001'
 # out_file='dab_ew11_dcc002'
 # out_file='dab_ew12_dcc007'
 # out_file='dab_ew13_dcc006'
-out_file = "dab_ew14_dcc005"
+# out_file = "dab_ew14_dcc005"
 port = 18002
 
 c = GtRecorderOriginal(

26953418e863012b310b291b230d31a100388405
delimarr <robin.delimar@students.fhnw.ch>
2023-11-30 14:53:53 +0100

    dab weichen neu messung run9

--- live.py
+++ live.py
@@ -5,13 +5,26 @@ from os import path
 
 from ebl_coords.backend.mock.gt_recorder_original import GtRecorderOriginal
 
-folder = "./data/got_raw_files/run8/"
-out_file = "16_dab"
+folder = "./data/got_raw_files/run9/"
 
+# out_file='dab_ew01_dcc019'
+# out_file='dab_ew02_dcc017'
+# out_file='dab_ew03_dcc018'
+# out_file='dab_ew04_dcc013'
+# out_file='dab_ew05_dcc014'
+# out_file='dab_ew06_dcc016'
+# out_file='dab_ew07_dcc015'
+# out_file='dab_ew08_dcc003'
+# out_file='dab_ew09_dcc004'
+# out_file='dab_ew10_dcc001'
+# out_file='dab_ew11_dcc002'
+# out_file='dab_ew12_dcc007'
+# out_file='dab_ew13_dcc006'
+out_file = "dab_ew14_dcc005"
 port = 18002
 
 c = GtRecorderOriginal(
-    out_file=path.join(folder, out_file), port=port, notebook_flg=False
+    out_file=path.join(folder, out_file), port=port, notebook_flg=False, max_rows=200
 )
 
 

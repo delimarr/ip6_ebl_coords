80eee33e9fa4af6a4ea31fa9d647d82e9efd8210
delimarr <robin.delimar@students.fhnw.ch>
2024-04-17 11:47:58 +0200

    removed bloat

--- ebl_coords/backend/constants.py
+++ ebl_coords/backend/constants.py
@@ -25,7 +25,6 @@ IGNORE_Z_AXIS: bool = True
 
 # minimal distance [m] delta needed for summation
 MIN_DELTA_DISTANCE: float = 0.03
-MIN_SHOW_DISTANCE: float = 0.1
 
 # GTCommand train switch hit threshold
 TS_HIT_THRESHOLD: int = 50
